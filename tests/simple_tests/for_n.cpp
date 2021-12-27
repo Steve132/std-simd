526c46da57b5535599989638298d54ab409bcd17
Steve132 <steve@soapforge.com>
2021-12-27 02:33:56 -0500

    Update for_n.cpp

--- tests/simple_tests/for_n.cpp
+++ tests/simple_tests/for_n.cpp
@@ -15,64 +15,101 @@ namespace stdx = std::experimental;
 
 
 #include<iostream>
+#include<numeric>
 
 
-//Process for at least N < 2*CurrentKernel-1
+//Process for at least N < 2*CurrentK-1
 template<size_t CurK,
 	size_t MinK=1,
 	class KernelStruct,
 	class ...Args>
-std::enable_if_t<(CurK < MinK),size_t>
-_for_n_end(size_t i,size_t N,KernelStruct&& ks,Args&&... args)
+static inline
+std::enable_if_t<((CurK/2) < MinK),size_t>
+_for_n_end(size_t i,size_t e,KernelStruct&& ks,Args&&... args)
 {
    return i; //error, didn't work, kernel too small
 }
+
 template<size_t CurK,
 	size_t MinK=1,
 	class KernelStruct,
 	class ...Args>
-std::enable_if_t<(CurK == MinK),size_t>
-_for_n_end(size_t i,size_t N,KernelStruct&& ks,Args&&... args)
+static inline
+std::enable_if_t<((CurK/2) >= MinK),size_t>
+_for_n_end(size_t i,size_t e,KernelStruct&& ks,Args&&... args)
 {
-	if(CurK <= N)
+	e &= (CurK-1);
+	if(e >= (CurK/2))
 	{
 		std::forward<KernelStruct>(ks).
-			template kernel<CurK,MinK>(i,N,
+			template kernel<CurK/2>(i,
 			std::forward<Args>(args)...);
-		i+=CurK;
+		i+=CurK/2;
+	}
+	i=_for_n_end<CurK/2>(i,e,ks,args...);
+	return i;
+}
+
+template<size_t CurK,
+	size_t MinK=1,
+	class KernelStruct,
+	class ...Args>
+static inline
+std::enable_if_t<((CurK/2) < MinK),size_t>
+_for_n_begin(size_t i,KernelStruct&& ks,Args&&... args)
+{
+	return i;
+}
+
+template<size_t CurK,
+	size_t MinK=1,
+	class KernelStruct,
+	class ...Args>
+static inline
+std::enable_if_t<((CurK/2) >= MinK),size_t>
+_for_n_begin(size_t i,KernelStruct&& ks,Args&&... args)
+{
+	i=_for_n_begin<CurK/2>(i,ks,args...);
+	if(i & (CurK/2))
+	{
+		std::forward<KernelStruct>(ks).
+			template kernel<CurK/2>(i,
+			std::forward<Args>(args)...);
+		i+=CurK/2;
 	}
 	return i;
 }
+
+
+//select a max size based on max value that N straddles.
 template<size_t CurK,
 	size_t MinK=1,
 	class KernelStruct,
 	class ...Args>
 std::enable_if_t<(CurK > MinK),size_t>
-_for_n_end(size_t i,size_t N,KernelStruct&& ks,Args&&... args)
+static inline
+_for_n(size_t i,size_t N,KernelStruct&& ks,Args&&... args)
 {
-	if(N >= CurK)
-	{
-		std::forward<KernelStruct>(ks).
-			template kernel<CurK,MinK>(i,N,
-			std::forward<Args>(args)...);
-		i+=CurK;
-		N-=CurK;
-	}
-	i=_for_n_end<CurK/2,MinK>(i,N,ks,args...);
-	return i;
+	if((i % MinK) || (N % MinK)) return 0;
+	if(CurK <= N)
+		return _for_n_internal<CurK,MinK>(i,N,ks,args...);
+	else
+		return _for_n<CurK/2,MinK>(i,N,ks,args...);
 }
+
+
 
 struct KernelTest
 {
-	template<size_t Cur,size_t Min>
-	void kernel(size_t i,size_t N)
+	template<size_t Cur>
+	void kernel(size_t i)
 	{
-		std::cout << "Chunk:" << Cur << ",i:" << i << ",N:" << N << std::endl;
+		std::cout << "Chunk:" << i << "-" << i+Cur << "(" << Cur << ")" << std::endl;
 	}
 };
 int main()
 {
-	_for_n_end<128>(0,129,KernelTest{});
+	_for_n_begin<64>(3,KernelTest{});
 
 	return 0;
 }
